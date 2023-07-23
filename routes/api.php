ecc1ed9c1534c997967974005cda018342966088
hossam <hossam0soliuman@gmail.com>
2023-07-23 12:46:40 +0300

    edit api route and database  seeding

--- routes/api.php
+++ routes/api.php
@@ -13,7 +13,17 @@ use Illuminate\Support\Facades\Route;
 | is assigned the "api" middleware group. Enjoy building your API!
 |
 */
-
+//public routes
 Route::post('login',[AuthenticationController::class,'login']);
 Route::post('register',[AuthenticationController::class,'register']);
 
+
+//auth routes
+Route::middleware(['auth:sanctum'])->group(function(){
+    
+});
+
+//admin routes
+Route::middleware(['auth:sanctum','admin'])->group(function(){
+    
+});
