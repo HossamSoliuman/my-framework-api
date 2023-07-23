4abe4e365c6c0de7743f558bd6cb2bed985ab885
hossam <hossam0soliuman@gmail.com>
2023-07-23 12:38:45 +0300

    add CRUDs generator

--- routes/api.php
+++ routes/api.php
@@ -13,6 +13,7 @@ use Illuminate\Support\Facades\Route;
 | is assigned the "api" middleware group. Enjoy building your API!
 |
 */
+
 Route::post('login',[AuthenticationController::class,'login']);
 Route::post('register',[AuthenticationController::class,'register']);
 
