f83d886d9d85b9514205b367a7b16e8721b921b1
hossam <hossam0soliuman@gmail.com>
2023-08-16 15:49:46 +0300

    update Authentication logic

--- app/Http/Controllers/AuthenticationController.php
+++ app/Http/Controllers/AuthenticationController.php
@@ -4,23 +4,33 @@ namespace App\Http\Controllers;
 
 use App\Http\Requests\LoginRequest;
 use App\Http\Requests\RegisterRequest;
+use App\Http\Requests\UpdateAuthRequest;
+use App\Http\Requests\UpdateRequest;
+use App\Http\Resources\RegisterResource;
+use App\Http\Resources\UserResource;
+use App\Models\MembershipPlan;
+use App\Models\Usage;
 use App\Models\User;
+use App\Traits\ApiResponse;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Hash;
 
 class AuthenticationController extends Controller
 {
+    use ApiResponse;
 
     public function register(RegisterRequest $request)
     {
-        $user = User::create($request->validated());
+        $validData = $request->validated();
+        $validData['password'] = Hash::make($validData['password']);
+        $user = User::create($validData);
 
         $token = $user->createToken('auth_token')->plainTextToken;
-
         return $this->successResponse(
             [
                 'token' => $token,
-                'user' => $user,
+                'user' => UserResource::make($user),
             ],
         );
     }
@@ -30,7 +40,7 @@ class AuthenticationController extends Controller
         $validated = $request->validated();
 
         if (!Auth::attempt($validated)) {
-            return $this->errorResponse('User name or Password is wrong', 401);
+            return $this->errorResponse('Email or password is wrong', 401);
         }
 
         $user = User::where('email', $validated['email'])->first();
@@ -39,7 +49,7 @@ class AuthenticationController extends Controller
         return $this->successResponse(
             [
                 'token' => $token,
-                'user' => $user,
+                'user' => UserResource::make($user),
             ],
         );
     }
@@ -48,8 +58,29 @@ class AuthenticationController extends Controller
     {
         $request->user()->tokens()->delete();
 
-        return $this->messageResponse(
-            'Successfully logedout'
+        return $this->customResponse([], 'Successfully logged out');
+    }
+
+    public function update(UpdateAuthRequest $request)
+    {
+        $user = $request->user();
+        $validated = $request->validated();
+
+        if (isset($validated['email'])) {
+            $user->email = $validated['email'];
+        }
+
+        if (isset($validated['password'])) {
+            $user->password = Hash::make($validated['password']);
+        }
+
+        $user->save();
+
+        return $this->customResponse(
+            [
+                'user' => UserResource::make($user),
+            ],
+            'User updated successfully'
         );
     }
 }
